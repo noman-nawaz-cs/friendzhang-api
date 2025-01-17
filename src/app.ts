fa877c24a98eeef872fb8889980a3ad8eff90874
Noman Nawaz <noman.nawaz@ocloudsolutions.net>
2025-01-17 12:31:02 +0500

    changed the port

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import feedbackRoutes from "./routes/feedback.routes";
 import eventLoggerRoutes from "./routes/event-logger.routes";
 
 const app = express();
-const PORT = 80;
+const PORT = 3000;
 
 app.use(express.json());
 app.use(bodyParser.json());
